ed1eb033d70bef78b5672d67cba96d81912decd1
Seb <valdez.sebastian4@gmail.com>
2024-10-04 20:17:25 -0700

    Remove commented out code

--- main.go
+++ main.go
@@ -7,12 +7,4 @@ import "github.com/sebvaldez/weather-cli/cmd"
 
 func main() {
 	cmd.Execute()
-
-	// path, err := os.UserHomeDir()
-	// if err != nil {
-	// 	log.Fatal("something wrong with user config dir")
-	// }
-	// filepath
-
-	// fmt.Println("User home dir is: %v", path)
 }
